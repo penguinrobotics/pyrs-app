4786ec5846fd1429e71158720f1e5bcb0fdcff1d
kevqiu <kevinqiu96@gmail.com>
2025-11-24 01:41:09 -0800

    homepage: Update button styling

--- pages/index.js
+++ pages/index.js
@@ -15,26 +15,41 @@ const HomePage = () => {
         </Text>
         <Flex direction="column" gap="4" style={{ width: "100%", maxWidth: "400px" }}>
           <Link href="/referee" style={{ width: "100%" }}>
-            <Button size="4" style={{ width: "100%", height: "70px" }}>
-              Referee Page
+            <Button size="3" style={{ width: "100%", height: "80px" }}>
+              <Flex direction="column" align="center" gap="1">
+                <Text size="4" weight="bold">🏁 Referee</Text>
+                <Text size="1" style={{ opacity: 0.8 }}>Track on-field violations</Text>
+              </Flex>
             </Button>
           </Link>
           <Link href="/queue/admin" style={{ width: "100%" }}>
-            <Button size="4" style={{ width: "100%", height: "70px" }}>
-              Queue Admin Page
+            <Button size="3" style={{ width: "100%", height: "80px" }}>
+              <Flex direction="column" align="center" gap="1">
+                <Text size="4" weight="bold">⚙️ Queue Admin</Text>
+                <Text size="1" style={{ opacity: 0.8 }}>Manage skills queue</Text>
+              </Flex>
             </Button>
           </Link>
           <Link href="/queue/kiosk" style={{ width: "100%" }}>
-            <Button size="4" style={{ width: "100%", height: "70px" }}>
-              Kiosk
+            <Button size="3" variant="surface" style={{ width: "100%", height: "60px" }}>
+              <Flex direction="column" align="center" gap="1">
+                <Text size="4" weight="bold">🎫 Kiosk</Text>
+                <Text size="1" style={{ opacity: 0.8 }}>Skills registration view</Text>
+              </Flex>
             </Button>
           </Link>
           <Link href="/queue/current" style={{ width: "100%" }}>
-            <Button size="4" style={{ width: "100%", height: "70px" }}>
-              Current Queue
+            <Button size="3" variant="surface" style={{ width: "100%", height: "60px" }}>
+              <Flex direction="column" align="center" gap="1">
+                <Text size="4" weight="bold">📺 Current Queue</Text>
+                <Text size="1" style={{ opacity: 0.8 }}>Pit display view of queue</Text>
+              </Flex>
             </Button>
           </Link>
         </Flex>
+        <Text size="2" style={{ opacity: 0.6, marginTop: "2rem" }}>
+          💜 made w/ love by @kevqiu
+        </Text>
       </Flex>
     </>
   );
